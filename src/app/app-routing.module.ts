ae78b40b87dcf3445b15d8a47f78f79a778fbf4f
DESKTOP-DH1O5F3\PC <dzenis.brkan@outlook.com>
2024-05-11 19:24:10 +0200

    Main page design update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './component/details/details.component';
-import { SearchBarComponent } from './component/search-bar/search-bar.component';
-import { AppComponent } from './app.component';
-import { ShowsComponent } from './component/shows/shows.component';
 import { MoviesComponent } from './component/movies/movies.component';
 import { MoviesDetailsComponent } from './component/movies-details/movies-details.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { SearchBarComponent } from './component/search-bar/search-bar.component';
-import { ShowsComponent } from './component/shows/shows.component';
-import { TabSwitchComponent } from './component/tab-switch/tab-switch.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesComponent } from './component/movies/movies.component';
 import { MoviesDetailsComponent } from './component/movies-details/movies-details.component';
@@ -15,9 +12,6 @@ import { CardMovieComponent } from './component/card-movie/card-movie.component'
 @NgModule({
   declarations: [	
     AppComponent,
-    // SearchBarComponent,
-    // ShowsComponent,
-    // TabSwitchComponent,
     MoviesComponent,
     MoviesDetailsComponent,
     CardMovieComponent
